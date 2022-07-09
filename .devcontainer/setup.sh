32b364382502be07e6ecba2386b0a8f3232a44f8
Code IT <richard@codeit.ninja>
2022-07-09 15:38:01 +0200

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,62 +1,63 @@
-#! /bin/bash
-#
-# Inspired by https://github.com/helen/wcus-2021/tree/trunk/.devcontainer
-#
+#!  /bin/bash
 
-# Load environment variables from .devcontainer/.env
-set -o allexport
-source "${BASH_SOURCE%/*}/.env"
-set +o allexport
-
-if [[ ! -z "$CODESPACE_NAME" ]]; then
-	SITE_HOST="https://${CODESPACE_NAME}-8080.githubpreview.dev"
-else
-	SITE_HOST="http://localhost:8080"
-fi
+sudo chown www-data: -R /var/www/html
 
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>setup.log 2>&1
 
+SITE_HOST="https://${CODESPACE_NAME}-8080.githubpreview.dev"
+
 # Prepare a nice name from project name for the site title.
-function getTitleFromSlug() {
-	local _slug=${SLUG//-/ }
-	local __slug=${_slug//_/ }
-	local ___slug=($__slug)
-	echo "${___slug[@]^}"
+function getTitleFromSlug()
+{
+    local _slug=${SLUG//-/ }
+    local __slug=${_slug//_/ }
+    local ___slug=( $__slug )
+    echo "${___slug[@]^}"
 }
 
-source ~/.bashrc
+echo "Downloading WordPress"
 
-# Install dependencies
-cd /var/www/html/wp-content/${PROJECT_TYPE}s/${SLUG}/
-npm i && npm run build
+cd /var/www/html
 
-# Install WordPress and activate the plugin/theme.
-cd /var/www/html/
-echo "Setting up WordPress at $SITE_HOST"
-wp db reset --yes
-if [[ $IS_MULTISITE -eq 1 ]]; then
-	WPARG=--network
-	wp core multisite-install --url="$SITE_HOST" --title="$(getTitleFromSlug) Development" --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
-	wp core update --url="$SITE_HOST"
+rm -f wp-config.php
 
-	# Add multisite .htaccess to the root directory.
-	cp .devcontainer/.htaccess .htaccess
+wp core download --force
 
-	# Patch WordPress to allow using port 8080.
-	diff -u wp-admin/includes/network.php .devcontainer/patches/network.php | patch -p0 -i -
-	diff -u wp-includes/ms-settings.php .devcontainer/patches/ms-settings.php | patch -p0 -i -
-	diff -u wp-includes/ms-site.php .devcontainer/patches/ms-site.php | patch -p0 -i -
+wp config create --dbhost="db" --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --skip-check
 
-	# Add 100 sub sites.
-	for ((i = 1; i <= 100; i++)); do
-		site_num=$(printf "%03d" $i)
-		wp site create --slug="site-$site_num" --title="Test Site $site_num"
-	done
-else
-	wp core install --url="$SITE_HOST" --title="$(getTitleFromSlug) Development" --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
+echo "Install project dependencies"
+
+# Install required node version
+if [[ ! -z "$NODE_VERSION" ]] && [[ "$NODE_VERSION" != "latest" ]]
+then
+    source ~/.nvm/nvm.sh
+    nvm install $NODE_VERSION
+    nvm use $NODE_VERSION
+    nvm alias default $NODE_VERSION
 fi
 
-echo "Activate $SLUG"
-wp $PROJECT_TYPE activate $SLUG $WPARG
+cd /var/www/html/wp-content/${PROJECT_TYPE}s/$SLUG
+
+echo "Changed directory to $(pwd)"
+
+if [[ "$USE_NODE" == "true" ]]
+then
+    npm install
+fi
+
+if [[ "$USE_COMPOSER" == "true" ]]
+then
+    composer install
+fi
+
+echo "Setting up WordPress at $SITE_HOST"
+
+wp db check
+wp db reset --yes
+wp core install --url="$SITE_HOST" --title="$(getTitleFromSlug) Development" --admin_user="$ADMIN_USER" --admin_email="$ADMIN_EMAIL" --admin_password="$ADMIN_PASS" --skip-email
+
+echo "Activating theme/plugin"
+
+wp $PROJECT_TYPE activate $SLUG
